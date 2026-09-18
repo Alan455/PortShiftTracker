c7dfb7ca718d2b9e1010bce4d7c8befad903f550
AlanSorrenti <77885859+Alan455@users.noreply.github.com>
2026-09-18 02:19:59 +0200

    Bump app version to 0.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "it.alantamanti.portshifttracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "0.10.0"
+        versionCode = 15
+        versionName = "0.10.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
